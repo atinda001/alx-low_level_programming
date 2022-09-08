ce9024350f61eee6eca70c9c1c7917bf96037c22
Atinda <atindasamson@gmail.com>
2022-09-08 12:04:27 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/** 
+/**
  *int main-beginning of the program
  *
-*/
+ * retrun: always o successful
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/**
+	/*
 	 * return - returns a value of 0
 	 */
 	return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main -beginning of the program 
-*/
+ * main -Entry point
+ *
+ * Return:Always 0 (success)
+ */
 int main(void)
 {
 	int integerType;
@@ -9,15 +11,11 @@ int main(void)
 	long int longType;
 	char charType;
 	long long int longerInt;
-	/*
-	* print the values of the data types 
-	*/
-	printf("size of int: %ld \n", sizeof(integerType));
-	printf("size of a float: %ld \n", sizeof(floatType));
-	printf("size of a long int: %ld \n", sizeof(longType));
-	printf("sie of a long long int: %ld \n", sizeof(longerInt));
-	printf("size of a char: %ld \n", sizeof(charType));
-	/* return -returns a value of 0 */
+	printf("size of int: %ld byte(s)\n", sizeof(integerType));
+	printf("size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("size of a long int: %ld byte(s)\n", sizeof(longType));
+	printf("sie of a long long int: %ld byte(s)\n", sizeof(longerInt));
+	printf("size of a char: %ld byte(s)\n", sizeof(charType));
 	return (0);
 }
 
