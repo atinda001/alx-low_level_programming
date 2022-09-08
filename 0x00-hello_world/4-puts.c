768fbdb729d82fc1a9df0fa638767c89eadf31ce
Atinda <atindasamson@gmail.com>
2022-09-08 02:45:11 -0700

    puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main ()
+/** description 
+ * int main(void) beginning of code
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("'Programming is like building a multilingual puzzle\n");
+	return (0);
 }
