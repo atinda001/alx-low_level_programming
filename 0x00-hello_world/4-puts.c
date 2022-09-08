f1b2750379933e675c1476944f3e5c13bb257c39
Atinda <atindasamson@gmail.com>
2022-09-08 11:49:25 -0700

    put is put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main-beginning of code
- * return-returns 0 if succesful
+ * main-Entry point
+ *
+ * return:Returns o (succesful)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
