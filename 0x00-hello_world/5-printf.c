3de7de2b10f82428b78f18f00996358d097f6e50
Atinda <atindasamson@gmail.com>
2022-09-08 10:51:13 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ *int main-beginning of the program
+ *
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	 * return - returns a value of 0
+	 */
+	return (0);
+}
