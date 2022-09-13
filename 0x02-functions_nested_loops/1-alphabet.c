ca106c8bf315fc76d6e226765e8ae8a258f80b47
Atinda <atindasamson@gmail.com>
2022-09-13 08:49:50 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	void print_alphabet()
+	{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+	}
+	return (0);
+}
