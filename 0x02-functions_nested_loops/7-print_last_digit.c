8feb946e2ec8ae27cc8bfc74865a82e07ba08f8c
Atinda <atindasamson@gmail.com>
2022-09-14 02:13:14 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print_alphabet
+ *
+ * @x: int variable
+ *
+ * Return: last digit of x
+ */
+int print_last_digit(int x)
+{
+	int last_digit;
+
+	last_digit = x % 10;
+	return (last_digit);
+}
