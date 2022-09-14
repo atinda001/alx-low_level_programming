c421bc6097c7d5dbb875ed4786235752a6c167c8
Atinda <atindasamson@gmail.com>
2022-09-14 13:07:09 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@ void times_table(void)
 	int i, j;
 	int mult;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			mult = i * j;
 			if (j == 0)
