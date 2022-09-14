7e13772c387fb0a98d9321a8b6bfe4a66c8a048e
Atinda <atindasamson@gmail.com>
2022-09-14 02:45:36 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main"
+
+/**
+ * jack_bauer -print_hours
+ *
+ * @x,a: hours
+ * @y,b:minutes
+ *Return: all minutes of day
+ */
+
+void jack_bauer(void)
+{
+	int x, a;
+	int z, b;
+
+	for (x = 0; x <= 2; x++)
+	{
+		for (a = 0; a <= 3; a++)
+		{
+			_putchar(x + a);
+		}
+	}
+	_putchar(':');
+
+	for (z = 0; z <= 5; z++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(z + b);
+		}
+	}
+}
