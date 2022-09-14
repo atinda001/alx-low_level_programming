70b279709438d034f7b63158636294af7fc25c2a
Atinda <atindasamson@gmail.com>
2022-09-14 12:26:28 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_to_98 - prints to 98
+ *
+ * @n: starting point of count
+ *
+ * Return: print count
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	for (x = n; x <= 98; x++)
+	{
+		_putchar(x);
+	}
+}
