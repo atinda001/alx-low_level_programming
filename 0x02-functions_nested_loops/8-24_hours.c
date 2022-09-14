a68072df428639c239d6335cb7f79e5f6a6aaf4c
Atinda <atindasamson@gmail.com>
2022-09-14 03:05:50 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,25 +10,25 @@
 
 void jack_bauer(void)
 {
-	int x, a;
-	int z, b;
+	int x, a = 0;
+	int z, b = 0;
 
-	while (x = 0; x <= 2)
+	while (x <= 2)
 	{
 		_putchar(x);
 		x++;
-	while (a = 0; a <= 3)
+	while (a <= 3)
 	{
 		_putchar(a);
 		a++;
 	_putchar(':');
 
-	while (z = 0; z <= 5)
+	while (z <= 5)
 	{
 		_putchar(z);
 		z++;
 	}
-	while (b = 0; b <= 9)
+	while (b <= 9)
 		{
 			_putchar(b);
 			b++;
