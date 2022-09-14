c6b7dbe1e678a6a7a40d6238521a2668604626cb
Atinda <atindasamson@gmail.com>
2022-09-14 12:01:37 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - print sum
+ *
+ * @x,y: integer variables
+ *
+ * Return: returns sum of x and y
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	printf("%d\n", sum);
+}
