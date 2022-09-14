af68e8b885abbeaec04845ff8d8f0fee3be7c615
Atinda <atindasamson@gmail.com>
2022-09-14 12:52:53 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,7 +21,7 @@ void print_to_98(int n)
 		}
 		else if (i == 98)
 		{
-			printf("%d,\n", i);
+			printf("%d\n", i);
 		}
 	}
 	}
@@ -35,7 +35,7 @@ void print_to_98(int n)
 			}
 			else if (j == 98)
 			{
-				printf("%d,\n", j);
+				printf("%d\n", j);
 			}
 		}
 }
