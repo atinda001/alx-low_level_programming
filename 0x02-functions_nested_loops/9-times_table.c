ea2a2c467843241289e763024f8748fcc82777af
Atinda <atindasamson@gmail.com>
2022-09-14 03:19:04 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,15 @@
  * Return: product table
  */
 
-void times_table(void)
+void times_table()
 {
-	int i,j = 0;
+	int i, j;
 	int product;
 
 	product = i * j;
-	for (; i < 9; i++0)
+	for (i = 0; i < 9; i++)
 	{
-		for (; j < 9; j++)
+		for (j = 0; j < 9; j++)
 		{
 			return (product);
 		}
