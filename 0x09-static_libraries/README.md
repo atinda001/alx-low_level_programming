8abeedc33c2c2f5cd954ac822b3beeec6b1eac21
Atinda <atindasamson@gmail.com>
2022-10-03 10:52:23 +0300

    libraries

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,8 @@
+libraries
+	-ststic libraries
+	-dynamic libraries
+differences between the two
+creating libraries
+linking
+loading libraries
+
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always  0 on success
+ */
+int main(void)
+{
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
