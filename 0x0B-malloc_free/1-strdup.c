5e66283eb8e6de8dcfa4e92ab89058435f848feb
Atinda <atindasamson@gmail.com>
2022-10-05 11:07:05 +0300

    str duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/*
- * _strdup - print pointer to duplicate of string
- *
- * @s: char
+/**
+ * _strdup - returns a pointer to a new string which is
+ * a duplicate of the string str
+ * @str: string
  * Return: copy of string
+ * the _strdup() function returns a pointer to the duplicated
  */
 char *_strdup(char *str)
 {
@@ -21,7 +22,7 @@ char *_strdup(char *str)
 	{
 		length++;
 	}
-	dup = (char*)malloc(sizeof(char) * (length + 1));
+	dup = (char *)malloc(sizeof(char) * (length + 1));
 	if (dup == NULL)
 	{
 		return (NULL);
