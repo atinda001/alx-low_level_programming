3198b82fb4f9f3a6051c4cc21298a62d1e1d5d7b
Atinda <atindasamson@gmail.com>
2022-10-12 05:10:27 +0300

    pre-processor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - run the program
+*
+*Return: Always 0 on success
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - run the program
+*
+*Return: Always 0 on success
+*/
+int main(void)
+{
+	printf("%s\n", FILE);
+	return (0);
+}
