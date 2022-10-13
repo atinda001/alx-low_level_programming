b11ad01a24d784c8e4c9df51328f1ab9b46846d6
Atinda <atindasamson@gmail.com>
2022-10-13 03:52:44 +0300

    structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+*init_dog - initializes struct dog
+*@name: parameter
+*@age: second
+*@owner: third
+*@d: pointer to 
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog;
+	d = & dog;
+	(*d).name;
+	(*d).age;
+	(*d).owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h
+#define _dog_h
+/**
+*struct dog - structure
+*@name: first member
+*@age: second member
+*@owner: third member
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif  /* _dog_h */
